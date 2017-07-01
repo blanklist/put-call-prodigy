bb15c91169b04b681dc8b8244a04ff1970b73f14
Joanna Joseph <elainefirefly@gmail.com>
2017-07-01 16:08:29 -0700

    Create stock model

---
+++ app/models/stock.rb
@@ -0,0 +1,29 @@
+require "httparty"
+
+class Stock < ApplicationRecord
+
+  ALPHAVANTAGE_BEGIN_URL = "https://www.alphavantage.co/query?function="
+  private_constant :ALPHAVANTAGE_BEGIN_URL
+
+  def initialize(symbol, time_interval)
+    @symbol = symbol
+    @time_interval = time_interval
+  end
+
+  def generate
+    url = @time_interval == "day" ? construct_day_url : construct_intraday_url
+    HTTParty.get(url).parsed_response
+  end
+
+  def construct_day_url
+    # https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo
+    # https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&outputsize=full&apikey=demo
+    "#{ALPHAVANTAGE_BEGIN_URL}TIME_SERIES_DAILY&symbol=#{@symbol}&apikey=UIN02QE1RVT5HUFL"
+  end
+
+  def construct_intraday_url
+    # https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=demo
+    # https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=15min&outputsize=full&apikey=demo
+    "#{ALPHAVANTAGE_BEGIN_URL}TIME_SERIES_INTRADAY&symbol=#{@symbol}&interval=#{@time_interval}&apikey=UIN02QE1RVT5HUFL"
+  end
+end
