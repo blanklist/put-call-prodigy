6f4440a9782ba66a054b8dd9d77e8d7f7f41409a
pentaquant <pjsmueller@gmail.com>
2017-06-30 19:36:36 -0700

    Adds basic Alpha API function, will build out tomorrow

--- Gemfile
+++ Gemfile
@@ -30,6 +30,12 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+#API STURFF
+
+gem 'dotenv-rails'
+
+gem 'httparty'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
---
+++ app/models/alpha_adapter.rb
@@ -0,0 +1,21 @@
+require 'httparty'
+
+class AlphaAdapter
+  include HTTParty
+
+  def self.search(ticker)
+    api_key = 'T16XX6XEII8AXKZJ'
+    up_function = 'TIME_SERIES_INTRADAY'
+    up_ticker = ticker.upcase
+    interval = 1
+    url = "http://www.alphavantage.co/query?function=#{up_function}&symbol=#{up_ticker}&interval=#{interval}min&apikey=#{api_key}"
+    response = HTTParty.get(url)
+    response.parsed_response
+    p response.parsed_response
+  end
+end
+
+
+# ENV['ALPHA_API']
+
+# up_function = function.split.join('_').upcase
