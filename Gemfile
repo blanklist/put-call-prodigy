49143766cfc7ef5c636d3aa589617f2d209f6a77
Joe Hiner <joehineriv@gmail.com>
2017-07-08 14:53:30 -0700

    creates contract form on asset show page for each saved asset. also creates simple user show page table

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
+gem 'table_for_collection'
+
 
 #API STURFF
 gem 'httparty'
--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -5,7 +5,14 @@ class AssetsController < ApplicationController
 		@assets = Asset.all
 	end
 
+	def new
+		# @contract = Contract.new
+		# redirect new_contract_path
+	end
+
 	def show
+		@contract = Contract.new
+    @current_time = Time.now.in_time_zone("Pacific Time (US & Canada)").to_s.split(" ").second
 		@asset = Asset.find(params[:id])
 		@most_recent_price = Asset.get_price(@asset.ticker, "2017-07-08 07:30:00")
 	end
