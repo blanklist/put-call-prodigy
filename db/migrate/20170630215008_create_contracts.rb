83e161b2967d82c3d2a46b5e6820fa8f16a756f9
Tim Parkyn <timparkyn@gmail.com>
2017-07-10 21:13:59 -0700

    Modify Contract and Asset table per schema

--- db/migrate/20170630215008_create_contracts.rb
+++ db/migrate/20170630215008_create_contracts.rb
@@ -1,12 +1,15 @@
 class CreateContracts < ActiveRecord::Migration[5.1]
   def change
     create_table :contracts do |t|
-      t.string :ticker
-      t.float :strike_price
-      t.float :spot_price
+      t.string  :ticker
+      t.float   :strike_price
+      t.float   :spot_price
       t.integer :interval
-      t.float :gain_loss
-      t.references :user
+      t.float   :gain_loss
+      t.datetime      :expiration_date
+      t.integer       :status
+      t.references    :user
+      t.references    :asset
       t.timestamps
     end
   end
--- db/migrate/20170708182901_create_assets.rb
+++ db/migrate/20170708182901_create_assets.rb
@@ -1,8 +1,19 @@
 class CreateAssets < ActiveRecord::Migration[5.1]
   def change
     create_table :assets do |t|
-    	t.string :ticker
-    	t.references :contract
+    	t.string    :ticker
+      t.string    :company_name
+      t.text      :description
+      t.decimal   :roc
+      t.decimal   :rsi
+      t.decimal   :stoch_d
+      t.decimal   :stoch_k
+      t.decimal   :bbands_low
+      t.decimal   :bbands_mid
+      t.decimal   :bbands_high
+      t.text      :price_data
+    	t.references   :contract
+
     	t.timestamps
     end
   end
