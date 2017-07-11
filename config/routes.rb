0d9d5d5d2a90c8a83045aa3f36c287f858044549
Tim Parkyn <timparkyn@gmail.com>
2017-07-10 21:49:05 -0700

    Add nested resources to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :contracts, :assets, :users
+  resources :users
+
+  resources :assets do
+    resources :contracts
+  end
 
 #Need to add more "excepts" to sessions
 
