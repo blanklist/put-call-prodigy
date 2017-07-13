cea17f9c7bbbd374552744a01d927047b2244016
blanklist <joehineriv@gmail.com>
2017-07-13 14:12:06 -0700

    de-bugged contracts

--- app/jobs/calculate_gain_loss_job.rb
+++ app/jobs/calculate_gain_loss_job.rb
@@ -4,15 +4,16 @@ class CalculateGainLossJob < ApplicationJob
 
   def perform(contract)
     sold_time = alpha_time_adjustment(contract.created_at.time + contract.interval.minutes)
-    p sold_time
     sold_price = Equity.get_price(contract.ticker, sold_time)
-    p sold_price
+    contract.update_attributes(:sold_price => sold_price)
     if contract.put_call = 'Call'
       gain_loss = sold_price.to_f - contract.strike_price.to_f
     else 
       gain_loss = contract.strike_price.to_f - sold_price.to_f
     end
-    return 0 if gain_loss < 0
+    if gain_loss < 0
+      gain_loss = 0
+    end
     contract.update_attributes(:gain_loss => gain_loss)
   end
 end
