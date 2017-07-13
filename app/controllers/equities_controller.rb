aafbf14b589131c18bfd75fbd2b150fa45ecc62c
Tim Parkyn <timparkyn@gmail.com>
2017-07-12 17:10:26 -0700

    Add formatting to various view pages

--- app/controllers/equities_controller.rb
+++ app/controllers/equities_controller.rb
@@ -8,7 +8,6 @@ class EquitiesController < ApplicationController
 			@equity = Equity.find_by(id: params[:id])
       @contracts = current_user.contracts.order("created_at desc").select{|contract| contract.ticker == @equity.ticker }
 			@contract = Contract.new
-	    @current_time = Time.now.in_time_zone("Pacific Time (US & Canada)").to_s.split(" ").second
 
     else
       redirect_to new_user_path
