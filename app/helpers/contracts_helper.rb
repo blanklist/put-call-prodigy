c79efa684ef8c1fc475c57cd7701e19c1d66ef21
blanklist <joehineriv@gmail.com>
2017-08-06 11:54:24 -0700

    adds market open functionality to contract creation and gain_loss calculation. updates seed file to dynamically update technical data for each equity and logic checks if equity exists in database.

--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -1,6 +1,16 @@
 module ContractsHelper	
-	def alpha_time_adjustment(purchase_time)
-		adjusted_time = purchase_time - 4.hours
-		formatted_time = adjusted_time.change(:sec => 0).strftime("%Y-%m-%d %H:%M:%S")
-	end
+  def alpha_time_adjustment(purchase_time)
+	adjusted_time = purchase_time - 4.hours
+	formatted_time = adjusted_time.change(:sec => 0).strftime("%Y-%m-%d %H:%M:%S")
+  end
+
+  def markets_open?
+  	today_date = Time.now.strftime("%Y-%m-%d")
+  	if alpha_time_adjustment(Time.now) > today_date + " 15:59:00" || Date.today.saturday? || Date.today.sunday?
+  	  false
+  	else
+  	  true
+  	end
+  end
+
 end
--- app/models/equity.rb
+++ app/models/equity.rb
@@ -20,35 +20,55 @@ class Equity < ApplicationRecord
   end
 
   def self.get_rsi(ticker)
-    AlphaAdapter.rsi(ticker)["Technical Analysis: RSI"]["2017-07-13 09:30"]["RSI"]
+    response = AlphaAdapter.rsi(ticker)
+    last_response_time = response["Meta Data"]["3: Last Refreshed"]
+    time_without_seconds = last_response_time.chomp[0..-4]
+    response["Technical Analysis: RSI"][time_without_seconds]["RSI"]
   end
 
   def self.get_mfi(ticker)
-    AlphaAdapter.mfi(ticker)["Technical Analysis: MFI"]["2017-07-07"]["MFI"]
+    response = AlphaAdapter.mfi(ticker)
+    last_response_time = response["Meta Data"]["3: Last Refreshed"]
+    response["Technical Analysis: MFI"][last_response_time]["MFI"]
   end
 
   def self.get_adx(ticker)
-    AlphaAdapter.adx(ticker)["Technical Analysis: ADX"]["2017-07-13 09:30"]["ADX"]
+    response = AlphaAdapter.adx(ticker)
+    last_response_time = response["Meta Data"]["3: Last Refreshed"]
+    time_without_seconds = last_response_time.chomp[0..-4]
+    response["Technical Analysis: ADX"][time_without_seconds]["ADX"]
   end
 
   def self.get_obv(ticker)
-    AlphaAdapter.obv(ticker)["Technical Analysis: OBV"]["2017-07-07"]["OBV"]
+    response = AlphaAdapter.obv(ticker)
+    last_response_time = response["Meta Data"]["3: Last Refreshed"]
+    response["Technical Analysis: OBV"][last_response_time]["OBV"]
   end
 
   def self.get_sma(ticker)
-    AlphaAdapter.sma(ticker)["Technical Analysis: SMA"]["2017-07-07"]["SMA"]
+    response = AlphaAdapter.sma(ticker)
+    last_response_time = response["Meta Data"]["3: Last Refreshed"]    
+    response["Technical Analysis: SMA"][last_response_time]["SMA"]
   end
 
   def self.get_ema(ticker)
-    AlphaAdapter.ema(ticker)["Technical Analysis: EMA"]["2017-07-07"]["EMA"]
+    response = AlphaAdapter.ema(ticker)
+    last_response_time = response["Meta Data"]["3: Last Refreshed"]
+    response["Technical Analysis: EMA"][last_response_time]["EMA"]
   end
 
   def self.get_stoch_d(ticker)
-    AlphaAdapter.stoch(ticker)["Technical Analysis: STOCH"]["2017-07-13 09:30"]["SlowD"]
+    response = AlphaAdapter.stoch(ticker)
+    last_response_time = response["Meta Data"]["3: Last Refreshed"]
+    time_without_seconds = last_response_time.chomp[0..-4]
+    response["Technical Analysis: STOCH"][time_without_seconds]["SlowD"]
   end
 
   def self.get_stoch_k(ticker)
-    AlphaAdapter.stoch(ticker)["Technical Analysis: STOCH"]["2017-07-13 09:30"]["SlowK"]
+    response = AlphaAdapter.stoch(ticker)
+    last_response_time = response["Meta Data"]["3: Last Refreshed"]
+    time_without_seconds = last_response_time.chomp[0..-4]
+    response["Technical Analysis: STOCH"][time_without_seconds]["SlowK"]
   end
 
 end
